d5925014fca9056be0ab589c4c71b99fce0e94de
Raul Luis <soyrauluis@outlook.com>
2022-02-19 16:58:27 -0400

    Implement routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,39 @@
-import Home from './pages/Home';
-import { InstantSearch } from 'react-instantsearch-hooks';
-import searchClient from './algolia/searchClient';
-import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './index.css';
 import './App.css';
-import 'react-toastify/dist/ReactToastify.css';
+
+import AddProductPage from './pages/AddProductPage';
+import ClientsHomePage from './pages/ClientsHomePage';
+import ClientsPage from './pages/ClientsPage';
+import EditProductPage from './pages/EditProductPage';
+import Home from './pages/Home';
+import POSAppBar from './components/POSAppBar';
+import ProductsHomePage from './pages/ProductsHomePage';
+import ProductsPage from './pages/ProductsPage';
+import { Router } from '@reach/router';
+import { ToastContainer } from 'react-toastify';
+import searchClient from './algolia/searchClient';
 
 function App() {
 	return (
 		<>
-			<InstantSearch searchClient={searchClient} indexName='dev_Products'>
-				<div className='h-screen'>
-					<Home />
-				</div>
-			</InstantSearch>
+			<POSAppBar />
+			<Router>
+				<Home path='/' />
+				<ProductsPage path='products'>
+					<ProductsHomePage path='/' />
+					<EditProductPage path=':id/edit' />
+					<AddProductPage path='new' />
+				</ProductsPage>
+
+				<ClientsPage path='clients'>
+					<ClientsHomePage path='/' />
+				</ClientsPage>
+			</Router>
 			<ToastContainer position='top-right' />
 		</>
 	);
