a21acd06a2eeaa5a23b5b7ec52eb1928368e5c54
Raul Luis <soyrauluis@outlook.com>
2022-02-19 17:04:39 -0400

    Update utils

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,6 +1,15 @@
-import { collection, getDocs } from 'firebase/firestore';
-import { db } from '../firebase';
+import {
+	collection,
+	doc,
+	getDoc,
+	getDocs,
+	serverTimestamp,
+	updateDoc,
+} from 'firebase/firestore';
+
 import { FixMeLater } from '../models';
+import { Product } from './../models';
+import { db } from '../firebase';
 
 export const getDataFromDB = async () => {
 	const querySnapshot = await getDocs(collection(db, 'products'));
@@ -9,4 +18,20 @@ export const getDataFromDB = async () => {
 		data.push(doc.data());
 	});
 	return data;
+};
+
+export const getDocByID = async (id: string, collectionName = 'products') => {
+	const docRef = doc(db, collectionName, id);
+	const docSnap = await getDoc(docRef);
+
+	if (docSnap.exists()) return docSnap.data() as Product;
+	else throw new Error('No such document');
+};
+
+export const deleteDocByID = async (
+	id: string,
+	collectionName = 'products'
+) => {
+	const docRef = doc(db, collectionName, id);
+	await updateDoc(docRef, { deleted: true, updatedAt: serverTimestamp() });
 };
