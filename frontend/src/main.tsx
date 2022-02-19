2da1b87b80adf5f08ecd9cb93002245b4b1e306a
Raul Luis <soyrauluis@outlook.com>
2022-02-19 16:59:04 -0400

    Configure Material UI Theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import {
+	createTheme,
+	CssBaseline,
+	LinkProps,
+	ThemeProvider,
+} from '@mui/material';
+import { green, purple } from '@mui/material/colors';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import LinkBehavior from './components/mui/LinkBehavior';
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#00bfa5',
+		},
+		secondary: {
+			main: '#a7ffeb',
+		},
+	},
+	components: {
+		MuiLink: { component: LinkBehavior } as LinkProps,
+	},
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>,
+	document.getElementById('root')
+);
