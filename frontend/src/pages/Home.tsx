204cf0a8910cf146a857056574d7e0539d8c8fe0
Raul Luis <soyrauluis@outlook.com>
2022-02-19 17:04:26 -0400

    Update Home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,39 +1,18 @@
-import { useEffect, useState } from 'react';
-import ProductsPage from './ProductsPage';
-import { getDataFromDB } from '../utils';
-import AddProduct from '../components/AddProduct';
-import { toast } from 'react-toastify';
+import { Grid } from '@mui/material';
+import { RouteComponentProps } from '@reach/router';
 
-const views = ['Productos', 'Pedidos', 'Pagar', 'Agregar producto'];
+interface Props extends RouteComponentProps {}
 
-export default function Home() {
-	const [products, setProducts] = useState<any[]>([]);
-	const [view, setView] = useState('');
-
-	useEffect(() => {
-		const getAndSetData = async () => {
-			setProducts(await getDataFromDB());
-		};
-		getAndSetData();
-	}, []);
-
+export default function Home({}: Props) {
 	return (
-		<div className='flex flex-col h-full justify-end'>
-			<nav className='order-last flex justify-around bg-gray-100 py-6 text-purple-500 px-12 text-lg'>
-				{views.map((viewLink) => (
-					<button
-						className={`${
-							view === viewLink ? 'text-purple-500' : 'text-gray-800'
-						} h-full`}
-						onClick={(e) => setView(viewLink)}>
-						{viewLink}
-					</button>
-				))}
-			</nav>
-			<div className='h-full'>
-				{view === 'Productos' && <ProductsPage products={products} />}
-				{view === 'Agregar producto' && <AddProduct />}
-			</div>
+		<div className=''>
+			<Grid container justifyContent='center' alignItems={'center'}>
+				<Grid item xs={6}>
+					<h1 className=''>
+						Bienvenido al sistema <span className=''>POS</span>
+					</h1>
+				</Grid>
+			</Grid>
 		</div>
 	);
 }
