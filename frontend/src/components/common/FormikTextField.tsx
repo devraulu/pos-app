c599963fdf554012b059ad0672626da850dc3c69
Raul Luis <soyrauluis@outlook.com>
2022-02-27 20:59:34 -0400

    Update FormikTextField

--- frontend/src/components/common/FormikTextField.tsx
+++ frontend/src/components/common/FormikTextField.tsx
@@ -1,22 +1,27 @@
+import { Client, Product } from 'models';
 import { Field, useFormikContext } from 'formik';
 
-import { Product } from '../../models';
 import { TextField } from '@mui/material';
 
 type Props = {
 	name: string;
 	label: string;
+	type?: string;
 };
 
-export default function FormikTextField({ name, label }: Props) {
-	const { values, handleChange, errors, touched } = useFormikContext();
+export default function FormikTextField({ name, label, type = 'text' }: Props) {
+	const { values, handleChange, handleBlur, errors, touched } =
+		useFormikContext<any>();
+
 	return (
 		<TextField
 			id={name}
 			name={name}
 			label={label}
+			type={type}
 			value={(values as Product)[name as keyof Product]}
 			onChange={handleChange}
+			onBlur={handleBlur}
 			error={touched[name] && Boolean(errors[name])}
 			helperText={touched[name] && errors[name]}
 		/>
