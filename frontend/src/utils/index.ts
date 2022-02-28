a7c23e1133b138ee3583fea6084337b5bcbad8d6
Raul Luis <soyrauluis@outlook.com>
2022-02-27 20:42:05 -0400

    Add format helper functions and uuid

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,3 +1,4 @@
+import { Client, FixMeLater } from 'models';
 import {
 	collection,
 	doc,
@@ -7,9 +8,10 @@ import {
 	updateDoc,
 } from 'firebase/firestore';
 
-import { FixMeLater } from '../models';
-import { Product } from './../models';
-import { db } from '../firebase';
+import { GridCellValue } from '@mui/x-data-grid';
+import { Product } from 'models';
+import { db } from 'firebase';
+import { v4 } from 'uuid';
 
 export const getDataFromDB = async () => {
 	const querySnapshot = await getDocs(collection(db, 'products'));
@@ -35,3 +37,30 @@ export const deleteDocByID = async (
 	const docRef = doc(db, collectionName, id);
 	await updateDoc(docRef, { deleted: true, updatedAt: serverTimestamp() });
 };
+
+export const formatDGCellToCurrency = (value: GridCellValue) =>
+	Number(value).toLocaleString('en-US', {
+		style: 'currency',
+		currency: 'USD',
+	});
+
+export const formatClient = (values: Client) => ({
+	...values,
+	name: values.name.toUpperCase(),
+	address: values.address.toUppercase(),
+	email: values.email.toLowerCase(),
+	createdAt: serverTimestamp(),
+	updatedAt: serverTimestamp(),
+});
+
+export const formatProduct = (values: Product) => ({
+	...values,
+	name: values.name.toUpperCase(),
+	category: values.category.toUpperCase(),
+	img:
+		values.img ||
+		'https://via.placeholder.com/150/000000/FFFFFF/?text=' + values.name,
+	updatedAt: serverTimestamp(),
+});
+
+export const uuid = v4;
