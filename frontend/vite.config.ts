bac547d52f341ee94f76b0baa215613628f6bb3f
Raul Luis <soyrauluis@outlook.com>
2022-02-27 20:19:33 -0400

    Add MUI DataGrid, uuid and configure paths

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,6 @@ export default defineConfig({
 				plugins: ['@emotion/babel-plugin'],
 			},
 		}),
+		tsconfigPaths(),
 	],
 });
