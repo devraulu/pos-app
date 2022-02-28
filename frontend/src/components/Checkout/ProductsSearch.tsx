fb72ff78682ff4aaf13743b12402d4f73707f449
Raul Luis <soyrauluis@outlook.com>
2022-02-27 21:02:58 -0400

    Add ProductsSearch

---
+++ frontend/src/components/Checkout/ProductsSearch.tsx
@@ -0,0 +1,6 @@
+import ProductsInstantSearch from 'components/Products/ProductsInstantSearch';
+import { SearchBox } from 'components/common/SearchBox';
+
+export default function ProductsSearch() {
+	return <SearchBox placeholder='Search for a product...' />;
+}
