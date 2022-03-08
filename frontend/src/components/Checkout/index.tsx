a815037b06b7b35b5d31e09b4b67ddcd548ac2d6
Raul Luis <soyrauluis@outlook.com>
2022-03-08 17:00:12 -0400

    Update Checkout

--- frontend/src/components/Checkout/index.tsx
+++ frontend/src/components/Checkout/index.tsx
@@ -1,41 +1,29 @@
 import { Box, Grid } from '@mui/material';
 
 import BarcodeScanner from './BarcodeScanner';
+import Cart from './Cart';
 import CheckoutHits from './CheckoutHits';
+import ClientInfo from './ClientInfo';
+import ClientsInstantSearch from 'components/Clients/ClientsInstantSearch';
+import ClientsSearch from './ClientsSearch';
+import Menu from './Menu';
+import Order from './Order';
 import ProductsInstantSearch from 'components/Products/ProductsInstantSearch';
 import ProductsSearch from './ProductsSearch';
 import { css } from '@emotion/react';
 
 export default function Checkout() {
 	return (
-		<Grid container spacing='2' sx={{ p: 4 }}>
-			<Grid item xs={8}>
-				<Box>
-					<ProductsInstantSearch>
-						<Box sx={{ display: 'flex', alignItems: 'center' }}>
-							<ProductsSearch />
-							<BarcodeScanner
-								cssProp={css`
-									margin-left: 1rem;
-									width: 100px;
-									height: 100px;
-								`}
-							/>
-						</Box>
-						<CheckoutHits />
-					</ProductsInstantSearch>
-				</Box>
-			</Grid>
-			<Grid item xs={4}>
-				<Box
-					sx={
-						{
-							// backgroundColor: 'primary.dark',
-							// heigth: 100,
-							// width: 100,
-						}
-					}></Box>
-			</Grid>
+		<Grid container spacing='2' p={2} className='checkout-wrapper'>
+			<ProductsInstantSearch>
+				<Grid item xs={5} display={'flex'} flexDirection='column'>
+					<Order />
+				</Grid>
+
+				<Grid item xs={8}>
+					<Menu />
+				</Grid>
+			</ProductsInstantSearch>
 		</Grid>
 	);
 }
