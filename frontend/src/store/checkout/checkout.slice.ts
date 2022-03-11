e97338ed12e4ca80d664d194c456e3a835854610
Raul Luis <soyrauluis@outlook.com>
2022-03-11 02:27:43 -0400

    Update checkout.slice.ts

--- frontend/src/store/checkout/checkout.slice.ts
+++ frontend/src/store/checkout/checkout.slice.ts
@@ -1,16 +1,15 @@
 import { Client, Product } from 'models';
-import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-
-import { RootState } from 'store';
-import { handleAddToCart } from './checkout.helpers';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 
 type ProductState = { count: number } & Product;
 
 interface CheckoutState {
 	products: ProductState[];
+	client?: Client;
 }
 const initialState: CheckoutState = {
 	products: [],
+	client: undefined,
 };
 export const checkoutSlice = createSlice({
 	name: 'checkout',
@@ -23,11 +22,29 @@ export const checkoutSlice = createSlice({
 			if (index !== -1) state.products[index].count += 1;
 			else state.products.push({ ...action.payload, count: 1 });
 		},
+		removeOneFromCart: (state, action: PayloadAction<ProductState>) => {
+			const index = state.products.findIndex(
+				(item) => item.objectID === action.payload.objectID
+			);
+			if (index !== -1) state.products[index].count -= 1;
+		},
+		removeFromCart: (state, action: PayloadAction<ProductState>) => {
+			const index = state.products.findIndex(
+				(item) => item.objectID === action.payload.objectID
+			);
+			if (index !== -1) state.products.splice(index, 1);
+		},
+		resetCart: (state) => {
+			state.products = [];
+			state.client = undefined;
+		},
+		setClient: (state, action: PayloadAction<Client>) => {
+			state.client = action.payload;
+		},
 	},
 });
 
-export const { addToCart } = checkoutSlice.actions;
-
-// export const selectClient = (state: RootState) => state?.checkout?.clientID;
+export const { addToCart, removeOneFromCart, removeFromCart, setClient } =
+	checkoutSlice.actions;
 
 export default checkoutSlice.reducer;
