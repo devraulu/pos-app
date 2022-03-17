f735f0fd7e9eaf80171cfeabcb2b92afcd5036e5
Raul Luis <soyrauluis@outlook.com>
2022-03-17 10:41:19 -0400

    Create CustomPagination

---
+++ frontend/src/components/common/CustomPagination.tsx
@@ -0,0 +1,30 @@
+import {
+	gridPageCountSelector,
+	useGridApiContext,
+	useGridSelector,
+} from '@mui/x-data-grid';
+
+import { Pagination } from '@mui/material';
+
+type Props = {
+	page: number;
+	handlePageChange: (page: number) => void;
+};
+
+export function CustomPagination({ page, handlePageChange }: Props) {
+	const apiRef = useGridApiContext();
+	const pageCount = useGridSelector(apiRef, gridPageCountSelector);
+
+	return (
+		<Pagination
+			color='primary'
+			count={pageCount}
+			page={page + 1}
+			shape='rounded'
+			onChange={(event, value) => {
+				console.log('handlePageChange props: ', value - 2);
+				handlePageChange(value - 2);
+			}}
+		/>
+	);
+}
