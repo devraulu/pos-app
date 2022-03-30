24e557bdb7eed0731bf9460aaef18bb23d9e0cb8
Raul Luis <soyrauluis@gmail.com>
2022-03-30 00:25:32 -0400

    Fix schemas

--- frontend/src/schemas.ts
+++ frontend/src/schemas.ts
@@ -17,7 +17,7 @@ export const ProductSchema = yup.object().shape({
 });
 
 const phoneRegExp = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
-
+const numberOnlyRegex = /^[0-9]*$/;
 export const ClientSchema = yup.object().shape({
 	name: yup
 		.string()
@@ -29,19 +29,17 @@ export const ClientSchema = yup.object().shape({
 		.string()
 		.matches(phoneRegExp, 'No es un teléfono válido')
 		.required('El teléfono es requerido'),
-	rnc: yup.string().matches(/[0-9]/).min(10, 'Muy corto').max(10, 'Muy largo'),
 	address: yup
 		.string()
 		.min(10, 'Muy corto')
 		.max(100, 'Muy largo')
 		.required('La dirección es requerida'),
-	credit_limit: yup
-		.number()
-		.positive('Debe ser un número positivo')
-		.min(5000, 'Debe ser mayor a {0}')
-		.max(500000, 'Debe ser menor a {0}')
-		.required('El límite de crédito es requerido'),
-	balance: yup.number().required('El saldo es requerido'),
+	cardCode: yup
+		.string()
+		.matches(numberOnlyRegex, 'No es un codigo valido')
+		.required('El número de tarjeta de códigos es requerido'),
+	visits: yup.number().positive(),
+	points: yup.number().positive(),
 });
 
 export const LoginSchema = yup.object().shape({
